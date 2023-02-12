10d52f758bfd3c3bbdf9894e3dc94edfdfe8269a
zackarydevove <Zackary.devove@hotmail.com>
2023-02-12 23:10:13 +0100

    correct the react exercice

--- react/Tip_Calculator.js
+++ react/Tip_Calculator.js
@@ -1,45 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function TipCalculator() {
-
   const [bill, setBill] = useState(50);
   const [tip, setTip] = useState(18);
-  const [num, setNum] = useState(1);
+  const [people, setPeople] = useState(1);
 
-  const totalTip = (bill * tip) / 100
-  const tipPerPerson = totalTip / num
-
-  const handleBill = event => {
-    const value = event.target.value
-    setBill(value)
-  }
-
-  const handleTip = event => {
-    const value = event.target.value
-    setTip(value)
-  }
-
-  const handlePeople = event => {
-    const value = event.target.value
-    setNum(value)
-  }
-
+  const totalTip = ((bill * tip) / 100).toFixed(2)
+  const tipPerPerson = (totalTip / people).toFixed(2)
+  
   return (
     <>
       <label>
       Bill
-      <input type='number' value={bill} onChange={handleBill} />
+      <input type="number" value={bill} onChange={event => { setBill(parseInt(event.target.value))}}/>
       </label>
       <label>
       Tip Percentage
-      <input type='number' value={tip} onChange={handleTip} />
+      <input type="number" value={tip}  onChange={event => { setTip(parseInt(event.target.value))}}/>
       </label>
       <label>
       Number of People
-      <input type='number' value={num} onChange={handlePeople} />
+      <input type="number" value={people} onChange={event => { setPeople(parseInt(event.target.value))}}/>
       </label>
-      <p>Total Tip: ${totaltip}</p>
-      <p>Tip Per Person: ${tipPerPerson}</p>
+      <p>Total Tip: {totalTip > 0 ? `$${totalTip}` : '-'}</p>
+      <p>Tip Per Person: {tipPerPerson > 0 ? `$${tipPerPerson}` : '-'}</p>
     </>
   );
 }
